562548352296f8ce01e32bdb248b9cdeb084e387
sunchaser <anika.mclean@gmail.com>
2022-05-15 22:33:09 -0400

    add find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,22 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        // be sure to include its associated Products
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
